a448a0644fa10a3e976ceddc2200842e9c7c59ad
程新文 <chengxinwen2005@163.com>
2019-08-05 16:14:22 +0800

    test

--- README.md
+++ README.md
@@ -1,27 +1,12 @@
-# ArcgisTest
+# arcGis地图Demo
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.9.
+## 安装esri-loader  
+参考文章： https://github.com/Esri/angular-cli-esri-map  
+参考demo：https://stackblitz.com/edit/angular-cli-esri-map2?file=src/main.ts  
+- npm install --save esri-loader
+- npm install --save @types/arcgis-js-api
 
-## Development server
+## 修改tsconfig.app.json
+在compilerOptions中增加"types": ["arcgis-js-api"]
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
-
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+ng build ---prod --aot --build-optimizer --base-href /arcgis/ --deploy-url /arcgis/
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { loadModules } from 'esri-loader';
+import esri = __esri; // Esri TypeScript Types
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'arcgis-test';
+export class AppComponent implements OnInit {
+  // The <div> where we will place the map
+  @ViewChild('mapViewDiv') private mapViewEl: ElementRef;
+
+  private _basemap = 'streets';
+  private _zoom = 10;
+  private _center: Array<number> = [0.1278, 51.5074];
+  private _loaded = false;
+
+  async initializeMap() {
+    try {
+
+      // Load the modules for the ArcGIS API for JavaScript
+      const [EsriMap, EsriMapView, FeatureLayer] = await loadModules([
+        'esri/Map',
+        'esri/views/MapView',
+        'esri/layers/FeatureLayer'
+      ]);
+
+      // Configure the Map
+      const mapProperties: esri.MapProperties = {
+        basemap: this._basemap,
+        // basemap: { baseLayers: [new FeatureLayer("http://192.168.25.2/ArcGIS/rest/services/RQDT/MapServer")] },
+        layers: [new FeatureLayer({ url: "http://218.91.211.162:6080/arcgis/rest/services/CityLight1222/MapServer/2" })]
+      };
+
+      const map: esri.Map = new EsriMap(mapProperties);
+
+      // Initialize the MapView
+      const mapViewProperties: esri.MapViewProperties = {
+        container: this.mapViewEl.nativeElement,
+        center: this._center,
+        zoom: this._zoom,
+        map: map
+      };
+      const mapView: esri.MapView = new EsriMapView(mapViewProperties);
+      mapView.when(() => {
+        console.log('loaded');
+      });
+    } catch (error) {
+      console.log('EsriLoader: ', error);
+    }
+  }
+
+  ngOnInit(): void {
+    // Initialize MapView and return an instance of MapView
+    this.initializeMap();
+  }
 }
